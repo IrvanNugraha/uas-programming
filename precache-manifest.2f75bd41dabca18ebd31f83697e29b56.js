98de8152766ba5dc212c75f7f70f3994c28cae4a
RivanNugraha <43240547+RivanNugraha@users.noreply.github.com>
2020-08-27 03:17:14 +0700

    Updates

---
+++ precache-manifest.2f75bd41dabca18ebd31f83697e29b56.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229305de5e83cf976a3397c10b988613",
+    "url": "/uas-programming/index.html"
+  },
+  {
+    "revision": "a901f383d2e91b4a123a",
+    "url": "/uas-programming/static/css/main.cfa44a98.chunk.css"
+  },
+  {
+    "revision": "152bb874679685f0bb78",
+    "url": "/uas-programming/static/js/2.f7257774.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/uas-programming/static/js/2.f7257774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a901f383d2e91b4a123a",
+    "url": "/uas-programming/static/js/main.65de0244.chunk.js"
+  },
+  {
+    "revision": "a5ea2d9d03079582153e",
+    "url": "/uas-programming/static/js/runtime-main.a6b174bd.js"
+  },
+  {
+    "revision": "8b82f2589ec04afff91e60727bca0166",
+    "url": "/uas-programming/static/media/Slide1.8b82f258.png"
+  },
+  {
+    "revision": "3f2ba51d49ea3d7c911208667f730914",
+    "url": "/uas-programming/static/media/Slide2.3f2ba51d.png"
+  },
+  {
+    "revision": "8b530b6a86a22da24f147266382cd1f7",
+    "url": "/uas-programming/static/media/Slide_3.8b530b6a.png"
+  },
+  {
+    "revision": "fa69ae88d7c8521ab5dcb86f36246527",
+    "url": "/uas-programming/static/media/polo_yellow.fa69ae88.jpeg"
+  },
+  {
+    "revision": "d12fbd25ea2d2dfd5255bc3c31af51a9",
+    "url": "/uas-programming/static/media/react_cloth.d12fbd25.png"
+  }
+]);
